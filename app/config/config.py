f0e131bbd09fb55fab8a34c086c1d01933b451cd
vel-3633f <vel3633fu@gmail.com>
2025-05-13 22:19:11 +0900

    feature/チャットアプリを追加

---
+++ app/config/config.py
@@ -0,0 +1,19 @@
+import vertexai
+from dotenv import load_dotenv
+import os
+
+
+def get_config():
+    load_dotenv()
+    gcp_project_id = os.getenv("GCP_PROJECT_ID")
+    gcp_location = os.getenv("GCP_LOCATION")
+    model_name = os.getenv("VERTEX_AI_MODEL_NAME")
+
+    vertexai.init(project=gcp_project_id, location=gcp_location)
+
+    config_data = {
+        "gcp_project_id": gcp_project_id,
+        "gcp_location": gcp_location,
+        "model_name": model_name,
+    }
+    return config_data
