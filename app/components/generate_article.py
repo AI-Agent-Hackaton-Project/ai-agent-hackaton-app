8b7c76e03ebb5003eb1e7d5c62dd7a2daf52b1fb
vel-3633f <vel3633fu@gmail.com>
2025-05-14 21:34:15 +0900

    feature/県ごとの記事を作成する関数を作成

--- app/components/generate_article.py
+++ app/components/generate_article.py
@@ -1,10 +1,17 @@
 import streamlit as st
-from config.config import get_env_config
+from app.config.env_config import get_env_config
 from langchain_google_vertexai import ChatVertexAI
+import json
+from operator import itemgetter
+from langchain_community.retrievers import TavilySearchAPIRetriever
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from config.prompt_config import GENERATE_ARTICLE_PROMPT
 
 
-def generate_article():
+def generate_article(selected_prefecture):
     settings = get_env_config()
+    retriever = TavilySearchAPIRetriever(k=3)
 
     llm = ChatVertexAI(
         model=settings["model_name"],
@@ -13,13 +20,43 @@ def generate_article():
         max_retries=6,
         stop=None,
     )
-    messages = [
-        (
-            "system",
-            "You are a helpful assistant that translates English to French. Translate the user sentence.",
-        ),
-        ("human", "I love programming."),
-    ]
 
-    ai_msg = llm.invoke(messages)
-    st.write(ai_msg.content)
+    system_template = "あなたはプロのWEBライターです。"
+
+    prompt = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", GENERATE_ARTICLE_PROMPT)]
+    )
+
+    retriever_chain = itemgetter("selected_prefecture") | retriever
+
+    chain = (
+        {
+            "context": retriever_chain,
+            "selected_prefecture": itemgetter("selected_prefecture"),
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    input_data = {
+        "selected_prefecture": selected_prefecture,
+    }
+
+    try:
+        response_str = chain.invoke(input_data)
+        return json.loads(response_str)
+    except json.JSONDecodeError:
+        print(
+            "⚠️ JSON パースに失敗しました。LLMの返答が JSON 形式ではない可能性があります。"
+        )
+        print("--- LLM Raw Response ---")
+        print(response_str)
+        print("--- End LLM Raw Response ---")
+        return {"error": "JSON Decode Error", "raw_response": response_str}
+    except Exception as e:
+        print(f"予期せぬエラーが発生しました: {e}")
+        import traceback
+
+        traceback.print_exc()
+        return {"error": "Unexpected Error", "details": str(e)}
